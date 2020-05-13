d3d410f5563326d42259afe4e9e57ce93fa67d37
Sheva393 <Sheva961@gmail.com>
2020-05-13 21:37:45 +0300

    accum

--- tasks/accum.js
+++ tasks/accum.js
@@ -1,10 +1,7 @@
 function accum(arr){
     let str=arr.toUpperCase().split("");
-    
-   
+
   return str.reduce((acc,item,index) => acc+"-"+item+item.repeat(index).toLowerCase());
-   
-  
-   
   }
-  console.log(accum("Vlad"));
+  
+  console.log(accum("abcde"));
